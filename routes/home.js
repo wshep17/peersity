6701e1df54c6b85029b54c3d7fea43c3414a69c1
wshep17 <42911642+wshep17@users.noreply.github.com>
2019-09-17 07:20:21 -0500

    removed braintree functionality

--- routes/home.js
+++ routes/home.js
@@ -4,33 +4,10 @@ var mongoUtil = require('../mongoUtil');
 var db = mongoUtil.getDb();
 var socket = require('socket.io');
 var cors = require('cors')
-var request = require('request');
-var header;
 
 /* GET root page for the home route. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
-	header = req.param('code');
-	if (header) {
-	    var data = {
-		  'client_secret': 'sk_test_Tfi92VcscIR3G8fqFWxxBsIu00Y7QAmHNW',
-		  'code': header,
-		  'grant_type': 'authorization_code'
-		}
-		request.post('https://connect.stripe.com/oauth/token', data=data, function(response) {
-			console.log(response)
-		})
-	}
-	
 	res.render('home', { title: 'Home' });
-});
-
-router.post('/oauthComplete', ensureAuthenticated, function (req, res, next) {
-
-	res.render('home');
-})
-
-router.get('/test', ensureAuthenticated, function(req, res, next) {
-	res.redirect('https://dashboard.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_Fd4RNlLyzXFDVNvZSmI5cSSRyZ4LDSuN&scope=read_write&redirect_uri=http://localhost:6969/oauthComplete');
 });
 
 router.post('/available', ensureAuthenticated, (req,res,next) => {
@@ -40,10 +17,6 @@ router.post('/available', ensureAuthenticated, (req,res,next) => {
 	db.collection('DefaultUser').update({_id: req.user._id}, {$set : {room: room}});
 	res.redirect('../home');
 })
-
-// router.post('/credentials', ensureAuthenticated, function(req, res, next) {
-
-// })
 
 
 function ensureAuthenticated(req, res, next) {
